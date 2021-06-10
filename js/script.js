c67b6dd166f55a0702bfe470bb89f8ddd027a3c4
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-06-10 14:47:05 +0200

    quasi ok

--- js/script.js
+++ js/script.js
@@ -18,3 +18,54 @@ Ad esempio: Di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Proviamo prima con pochi numeri, inserire 86 numeri ogni volta potrebbe essere un po’ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 */
+
+var randomNumbersPc = [];
+
+//funzione per numero random
+function getRandom (min, max) {
+    if (isNaN(min) || isNaN(max)) {
+        alert("Non hai inserito un numero");
+      } else {
+    var random = Math.floor(Math.random() * (max - min + min) + 1);
+    return random;
+  }
+}
+
+while (randomNumbersPc.length < 16) {
+  var cpuRandomNumber = getRandom(1, 100);
+  if(randomNumbersPc.includes(cpuRandomNumber) == false) {
+    randomNumbersPc.push(cpuRandomNumber);
+  }
+}
+
+console.log(randomNumbersPc.sort());
+
+// Creo un array per i numeri dell'utente
+var numeriUtente = [];
+
+// Creo un ciclo per i numeri dell'utente che vanno pushati
+// solo se non li ripete e se inserisce un numero vietato
+// il gioco si ferma
+
+while (numeriUtente.length < 84) {
+  var numeroInserito = parseInt(prompt("inserisci qui il primo di 84 numeri"));
+
+  if (randomNumbersPc.includes(numeroInserito) == true) {
+    document.getElementById('output').innerHTML = ('BOOM! Hai perso');
+  } 
+  else if ( numeroInserito <= 0 || numeroInserito > 84 || isNaN(numeroInserito) ) {
+    userNumbers = parseInt(prompt('Il dato inserito deve essere un numero compreso tra 0 e ' + 84));
+  }
+   else if (randomNumbersPc.includes(numeroInserito) == false){
+   numeriUtente.push(numeroInserito);
+ }
+  else if (numeriUtente.includes(numeroInserito) == true){
+    alert('Hai già inserito questo numero, inseriscine uno nuovo');
+ } else if (userNumberCont.length == numbersToWin) {
+    alert('Hai inserito ' + numeriUtente.length + ' numeri, il numero massimo di tentativi. Hai vinto!');
+    console.log('Hai inserito ' + numeriUtente.length + ' numeri, il numero massimo di tentativi. Hai vinto!');
+  }
+}
+
+
+console.log(numeriUtente);
